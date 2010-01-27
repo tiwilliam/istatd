195b70e2a8c6c7615a7862eda63705f2591460da
nevali <mo@nevali.net>
2010-01-27 09:01:17 +0000

    Convert sources from ISO-8859-1 to UTF-8

--- libfslabel/reiser.c
+++ libfslabel/reiser.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009 William Tis‰ter.
+/* Copyright (c) 2009 William Tis√§ter.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
